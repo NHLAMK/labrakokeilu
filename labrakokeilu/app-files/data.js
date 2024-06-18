283a008ad966ef36fba936d92c822925e473276c
NHLAMK <niilo_haavikko@outlook.com>
2024-06-18 14:23:34 +0300

    Numero 1

---
+++ labrakokeilu/app-files/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pic_2024_04_16_11_11_47_20240618140745",
+      "name": "PIC_2024_04_16_11_11_47_20240618140745",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.513283148161289,
+          "pitch": 0.44299974666942177,
+          "title": "Possu",
+          "text": "Röhröh"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
